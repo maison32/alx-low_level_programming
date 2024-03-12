88f4a3f5d25e97c386f5cdfaaf12bcca3def5902
maison32 <cheruyottony@outlook.com>
2024-03-12 01:59:53 -0700

    0x0F. C - Function pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,38 +2,46 @@
 #include <stdlib.h>
 
 /**
- * main - check the code for Holberton School students.
- * @argc: argument count.
- * @argv: argument vector.
- *
- * Return: Always 0.
- */
-int main(int argc, char *argv[])
-{
-	char *opc = (char *) main;
-	int i, nbytes;
-
-	if (argc != 2)
+	 * main - prints its own opcodes
+	 * @argc: number of arguments
+	 * @argv: array of arguments
+	 *
+	 * Return: Always 0 (Success)
+	 */
+	int main(int argc, char *argv[])
 	{
-		printf("Error\n");
-		exit(1);
+		int bytes, i;
+		char *arr;
+
+
+		if (argc != 2)
+		{
+			printf("Error\n");
+			exit(1);
+		}
+
+
+		bytes = atoi(argv[1]);
+
+
+		if (bytes < 0)
+		{
+			printf("Error\n");
+			exit(2);
+		}
+
+
+		arr = (char *)main;
+
+
+		for (i = 0; i < bytes; i++)
+		{
+			if (i == bytes - 1)
+			{
+				printf("%02hhx\n", arr[i]);
+				break;
+			}
+			printf("%02hhx ", arr[i]);
+		}
+		return (0);
 	}
-
-	nbytes = atoi(argv[1]);
-
-	if (nbytes < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-
-	for (i = 0; i < nbytes; i++)
-	{
-		printf("%02x", opc[i] & 0xFF);
-		if (i != nbytes - 1)
-			printf(" ");
-	}
-
-	printf("\n");
-	return (0);
-}
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,29 +1,24 @@
 #include "function_pointers.h"
-
-/**
- * int_index - searches for an integer
- * @array: input integer array.
- * @size: size of the array.
- * @cmp: pointer to the function to be used
- * to compare values.
- *
- * Return: index of the first eement for which the cmp
- * function does not return 0. If no elements matches,
- * return -1. If size <= 0, return -1.
- */
-int int_index(int *array, int size, int (*cmp)(int))
-{
-	int i;
-
-	if (array && cmp)
+	/**
+	 * int_index - return index place if comparison = true, else -1
+	 * @array: array
+	 * @size: size of elements in array
+	 * @cmp: pointer to func of one of the 3 in main
+	 * Return: 0
+	 */
+	int int_index(int *array, int size, int (*cmp)(int))
 	{
-		if (size <= 0)
+		int i;
+
+
+		if (array == NULL || size <= 0 || cmp == NULL)
 			return (-1);
 
+
 		for (i = 0; i < size; i++)
+		{
 			if (cmp(array[i]))
 				return (i);
+		}
+		return (-1);
 	}
-
-	return (-1);
-}
