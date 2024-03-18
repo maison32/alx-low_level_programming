eff36cf64698a9e38b387a4851f08f92415d6f20
maison32 <cheruyottony@outlook.com>
2024-03-18 01:19:36 -0700

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,31 +1,33 @@
 #include "3-calc.h"
-	#include <stdlib.h>
-	/**
-	 * get_op_func - Selects the correct function to perform
-	 *               the operation asked by the user.
-	 * @s: The operator passed as argument.
-	 *
-	 * Return: A pointer to the function corresponding
-	 *         to the operator given as a parameter.
-	 */
-	int (*get_op_func(char *s))(int, int)
+
+/**
+ * get_op_func - funct pointer that selects the correct function to perform
+ * the operation asked by the user
+ * @s: the operator given by the user
+ *
+ * Return: pointer to the function that corresponds to the
+ * operator given as a parameter
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int a;
+
+	a = 0;
+
+	while (ops[a].op)
 	{
-		op_t ops[] = {
-			{"+", op_add},
-			{"-", op_sub},
-			{"*", op_mul},
-			{"/", op_div},
-			{"%", op_mod},
-			{NULL, NULL},
-		};
-
-
-		int i = 0;
-
-
-		while (ops[i].op != NULL && *(ops[i].op) != *s)
-			i++;
-
-
-		return (ops[i].f);
+		if (strcmp(ops[a].op, s) == 0)
+			return (ops[a].f);
+		a++;
 	}
+
+	return (NULL);
+}
